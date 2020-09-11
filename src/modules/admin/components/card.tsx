eface8fe6405a73ff63ff2c8cff793dcf8030950
lperezp <luiseperezp.10@gmail.com>
2020-09-10 22:41:52 -0500

    feat(components): add card component

--- src/modules/admin/components/card.tsx
+++ src/modules/admin/components/card.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './../../../assets/styles/scss/components/_cards.scss';
+import photo from './../../../post.jpg';
 
 const Card: React.FC = () => {
   return (
-    <div className='card-post'>
-      <p>sss</p>
+    <div className='card__post'>
+      <div className="post__header">
+        <img src={photo} alt="profile" className="post__header__photo" />
+        <label className="post__header__username">lperezp</label>
+      </div>
+      <img className="post__photo" src={photo} />
+      <div className="post__like">Les gusta a <span>lperezp</span></div>
+      <div className="post__comment"><span>lperezp</span> I like it!</div>
+      <small className="post__time">Les gusta a <span>lperezp</span></small>
     </div>
   );
 };
