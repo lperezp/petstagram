f51fa8ce87dca00a93acf5155e00faee1f20f212
lperezp <luiseperezp.10@gmail.com>
2020-09-10 20:48:57 -0500

    refactor(project): Fix styles components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Card from './modules/admin/components/card';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Petstagram</h1>
+      <Card />
     </div>
   );
 }
