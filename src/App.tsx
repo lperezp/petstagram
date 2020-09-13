bacb397ffbeaa13b59cd998a8b14fed3343233c7
lperezp <luiseperezp.10@gmail.com>
2020-09-13 01:24:17 -0500

    feat(components): add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.scss';
 import Card from './modules/admin/components/card';
 import History from './modules/admin/components/history';
+import NavBar from './modules/admin/components/NavBar';
 
 function App() {
   return (
     <div className='App'>
+      <NavBar />
       <History />
+      <Card />
       <Card />
       <Card />
       <Card />
---
+++ src/modules/admin/components/NavBar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './../../../assets/styles/scss/components/_navbar.scss';
+import logo_petstagram from './../../../assets/img/logo_petstagram.svg';
+import ic_paw from './../../../assets/img/ic_paw-outline.svg';
+import ic_home from './../../../assets/img/ic_home.svg';
+import ic_send from './../../../assets/img/ic_send.svg';
+import profile from './../../../assets/img/profile.jpg';
+
+const NavBar: React.FC = () => {
+  return (
+    <div className='container__navbar'>
+      <div className='box__navbar'>
+        <img
+          className='box__navbar__logo'
+          src={logo_petstagram}
+          alt='logo-petstagram'
+        />
+        <input
+          type='text'
+          className='box__navbar__search'
+          placeholder='Buscar'
+        />
+        <div className='navbar__icon'>
+          <img src={ic_home} alt='ic-home' />
+          <img src={ic_send} alt='ic-send' />
+          <img src={ic_paw} alt='ic-paw' />
+          <img src={profile} alt='profile' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
