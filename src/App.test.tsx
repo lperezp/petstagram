b2fafff5fb6837cfa216e6567633cb2e0e70644c
lperezp <luiseperezp.10@gmail.com>
2020-09-13 20:20:13 -0500

    perf(bundle): add webpack-analizer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,5 +3,6 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { getByText } = render(<App />);
 });
