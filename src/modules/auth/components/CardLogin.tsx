454da75fccff5ff982eaafc6e07469652872403b
lperezp <luiseperezp.10@gmail.com>
2020-09-19 20:18:55 -0500

    refactor(components): fix router login

--- src/modules/auth/components/CardLogin.tsx
+++ src/modules/auth/components/CardLogin.tsx
@@ -6,19 +6,28 @@ import ic_google from './../../../assets/img/ic_google.svg';
 import logo_petstagram from './../../../assets/img/logo_petstagram.svg';
 
 import 'firebase/auth';
+import { useHistory } from 'react-router-dom';
 
 const CardLogin: React.FC = () => {
+  const history = useHistory();
   const signInGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase
       .auth()
       .signInWithPopup(provider)
-      .then((result) => console.log(result))
-      .catch((e) => console.log(e));
+      .then((result) => {
+        console.log(result);
+        history.push('/home');
+      })
+      .catch((e) => console.log('error', e));
   };
   return (
     <div className='card card__login'>
-      <img src={logo_petstagram} alt='logo.petstagram' />
+      <img
+        src={logo_petstagram}
+        className='card__login__logo'
+        alt='logo.petstagram'
+      />
       <p>Iniciar sesión con Google</p>
       <button
         className='btn-primary'
